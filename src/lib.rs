da2cd91e7ca674a961b9d7ddedfbf8f89ec23b38
Ryo Aita <792803+aita@users.noreply.github.com>
2022-02-03 18:31:20 +0900

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use pyo3::prelude::*;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+#[pyfunction]
+#[pyo3(name = "poyo")]
+fn poyo_() -> PyResult<String> {
+    Ok("poyo".to_string())
+}
+
+#[pyfunction]
+fn sushi() -> PyResult<String> {
+    Ok("sushi".to_string())
+}
+
+#[pyfunction]
+fn sushi_or_poyo() -> PyResult<String> {
+    let choices = ["poyo", "sushi"];
+    let mut rng = thread_rng();
+    Ok(choices.choose(&mut rng).unwrap().to_string())
+}
+
+#[pymodule]
+fn poyo(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(poyo_, m)?)?;
+    m.add_function(wrap_pyfunction!(sushi, m)?)?;
+    m.add_function(wrap_pyfunction!(sushi_or_poyo, m)?)?;
+    Ok(())
+}
